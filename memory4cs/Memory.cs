314ed46018af332ffffd4db03aae97c4dc287899
cignoir <cignoir@gmail.com>
2014-04-03 16:02:20 +0900

    bugfix

--- memory4cs/Memory.cs
+++ memory4cs/Memory.cs
@@ -31,39 +31,37 @@ namespace memory4cs
             return process != null ? process.MainModule.BaseAddress.ToInt32() : 0;
         }
 
-        public byte[] Read(int address, int size, int baseAddress = 0)
+        public byte[] Read(int address, int size)
         {
             var buffer = new byte[size];
-            var absoluteAddress = baseAddress == 0 ? BaseAddress() + address : address;
-            Win32API.ReadProcessMemory(process.Handle.ToInt32(), absoluteAddress, buffer, size, 0);
+            Win32API.ReadProcessMemory(process.Handle.ToInt32(), address, buffer, size, 0);
             return buffer;
         }
 
-        public string ReadString(int address, int size, int baseAddress = 0)
+        public string ReadString(int address, int size)
         {
-            return Encoding.Unicode.GetString(Read(address, size, baseAddress));
+            return Encoding.Unicode.GetString(Read(address, size));
         }
 
-        public int ReadInt(int address, int size, int baseAddress = 0)
+        public int ReadInt(int address, int size)
         {
-            var bytes = Read(address, size, baseAddress);
+            var bytes = Read(address, size);
             return int.Parse(BitConverter.ToString(bytes).Replace("-", ""), NumberStyles.HexNumber);
         }
 
-        public void Write(int address, byte[] bytes, int baseAddress = 0)
+        public void Write(int address, byte[] bytes)
         {
-            var absoluteAddress = baseAddress == 0 ? BaseAddress() + address : address;
-            Win32API.WriteProcessMemory(process.Handle.ToInt32(), absoluteAddress, bytes, bytes.Length, 0);
+            Win32API.WriteProcessMemory(process.Handle.ToInt32(), address, bytes, bytes.Length, 0);
         }
 
-        public void WriteString(int address, string str, int baseAddress = 0)
+        public void WriteString(int address, string str)
         {
-            Write(address, Encoding.Unicode.GetBytes(str + "\0"), baseAddress);
+            Write(address, Encoding.Unicode.GetBytes(str + "\0"));
         }
 
-        public void WriteInt(int address, int num, int baseAddress = 0)
+        public void WriteInt(int address, int num)
         {
-            WriteString(address, Convert.ToString(num, 16), baseAddress);
+            WriteString(address, Convert.ToString(num, 16));
         }
     }
         
